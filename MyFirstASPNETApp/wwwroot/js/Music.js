67b2f9d005c22f2b30b820d5f7cdd0282f31d186
gwolf17 <90425429+gwolf17@users.noreply.github.com>
2022-01-06 20:32:49 -0700

    Add project files.

---
+++ MyFirstASPNETApp/wwwroot/js/Music.js
@@ -0,0 +1,10 @@
+﻿////document.getElementById("btnAdd").addEventListener("click", function () {
+////    alert('"' + document.getElementById("title").value + '"' + ' is level: ' + document.getElementById("difficulty").value);
+////})
+
+$("#btnAdd").click(function () {
+    alert('"' + $("#title").val() + '"'
+        + ' is level: ' + $("#difficulty").val());
+
+    $("#pianoimg").fadeToggle();
+})  
